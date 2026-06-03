e14e48064fdb20b327fdb0b1d4c7b21ff59f2075
nayon-nillow <daringonline100@gmail.com>
2026-06-03 11:49:02 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -3,9 +3,14 @@
  const db = new Sequelize('defaultdb', 'avnadmin', 'AVNS_39mweOMP94ZjmkVbaQP', {
      host: "mysql-3b3a52ce-weblanjut12.h.aivencloud.com",
      dialect: "mysql",
-     port: 23415
-      "define": {
-        "timestamps": false
-      }
-    });
+     port: 23415,
+  dialectOptions: {
+ssl: {
+rejectUnauthorized: false
+ }
+ },
+define: {
+timestamps: false
+ }
+});
 export default db;
