1fb607ba440682228eb22f2941f911797fca779b
nayon-nillow <daringonline100@gmail.com>
2026-06-03 11:36:20 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
  import { Sequelize } from "sequelize";
  import mysql2 from "mysql2";
- const db = new Sequelize('bukumahapro', 'avnadmin', 'AVNS_39mweOMP94ZjmkVbaQP', {
+ const db = new Sequelize('defaultdb', 'avnadmin', 'AVNS_39mweOMP94ZjmkVbaQP', {
      host: "mysql-3b3a52ce-weblanjut12.h.aivencloud.com",
      dialect: "mysql",
      port: 23415
